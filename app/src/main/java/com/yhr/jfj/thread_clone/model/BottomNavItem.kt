90dfda446664bf65d538e40c906f98cebd7f3657
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-05 16:54:10 +0600

    Define BottomNavItem for bottom navigation bar

---
+++ app/src/main/java/com/yhr/jfj/thread_clone/model/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package com.yhr.jfj.thread_clone.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val title: String,
+    val route: String,
+    val icon: ImageVector
+)
